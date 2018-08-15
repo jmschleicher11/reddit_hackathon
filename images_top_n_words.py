960e1ef0377c49fdb16b0c0f09357a47410b9659
Jillian Schleicher <jmschleicher11@gmail.com>
2018-08-14 20:53:19 -0700

    Playing with getting images from top 5 words using google_images_download

---
+++ images_top_n_words.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 20:16:22 2018
+
+@author: Floreana
+"""
+
+import pandas as pd
+from google_images_download import google_images_download
+import os
+
+pickle_loc = '/Users/Floreana/Desktop/new_df.pkl'
+reddit_df = pd.read_pickle(pickle_loc)
+
+# scrape images of each from google images
+n_images = 1
+response = google_images_download.googleimagesdownload()
+for row, words in enumerate(reddit_df['Top_N_words']):
+    arguments={'keywords':" ".join(words), 
+               'limit':n_images, 
+               'format':'jpg',
+               'prefix':str(row),
+               'image_directory':os.path.join('images')}
+    absolute_image_paths = response.download(arguments)

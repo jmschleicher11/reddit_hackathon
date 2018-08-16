11d8ded85920a086fc85dd32d84caef79aa46070
David Riebel <riebel.d@gmail.com>
2018-08-15 19:14:20 -0700

    none

--- images_top_n_words.py
+++ images_top_n_words.py
@@ -10,17 +10,29 @@ import pandas as pd
 from google_images_download import google_images_download
 import os
 
-pickle_loc = '/Users/Floreana/Desktop/new_df.pkl'
+pickle_loc = '10word_df.pkl'
 reddit_df = pd.read_pickle(pickle_loc)
 
 # scrape images of each from google images
 n_images = 1
 response = google_images_download.googleimagesdownload()
-for row, words in enumerate(reddit_df['Top_N_words']):
+for row, words in enumerate(reddit_df['top_n_lemma']):
+    print('proccesing {} of {}'.format(row, reddit_df.shape[0]))
     arguments={'keywords':" ".join(words), 
                'limit':n_images, 
                'format':'jpg',
                'prefix':str(row) + " ".join(words),
-               'image_directory':os.path.join('images'), 
+               'image_directory':os.path.join('lemma_images'), 
                '--safe_search':''}
     absolute_image_paths = response.download(arguments)
+    
+for row, words in enumerate(reddit_df['top_n_stem']):
+    print('proccesing {} of {}'.format(row, reddit_df.shape[0]))
+    arguments={'keywords':" ".join(words), 
+               'limit':n_images, 
+               'format':'jpg',
+               'prefix':str(row) + " ".join(words),
+               'image_directory':os.path.join('stem_images'), 
+               '--safe_search':''}
+    absolute_image_paths = response.download(arguments)
+    

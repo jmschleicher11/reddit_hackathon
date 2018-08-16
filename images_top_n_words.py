35cb50f0ecd306b1a320277d6b84b3a1fb7e36e3
Sean Markert <markertsean0@gmail.com>
2018-08-16 12:00:02 -0700

    Turned the image_top_n_words stuff into a function that will return the link to the image as a string. It will download the image, create a log file, use the log file, then delete both.

--- images_top_n_words.py
+++ images_top_n_words.py
@@ -10,19 +10,83 @@ import pandas as pd
 from google_images_download import google_images_download
 import os
 
-pickle_loc = '/Users/Floreana/Desktop/new_df.pkl'
-reddit_df = pd.read_pickle(pickle_loc)
+def main():
+    pickle_loc = '/Users/Floreana/Desktop/new_df.pkl'
+    reddit_df = pd.read_pickle(pickle_loc)
 
-# scrape images of each from google images
-n_images = 1
-response = google_images_download.googleimagesdownload()
-for row, words in enumerate(reddit_df['top_n_lemma']):
-    print('proccesing {} of {}'.format(row, reddit_df.shape[0]))
-    arguments={'keywords':" ".join(words), 
-               'limit':n_images, 
-               'format':'jpg',
-               'prefix':str(row) + " ".join(words),
-               'image_directory':os.path.join('images'), 
-               'safe_search':True, 
-               'print_urls':True}
+    # scrape images of each from google images
+    n_images = 1
+    response = google_images_download.googleimagesdownload()
+    for row, words in enumerate(reddit_df['top_n_lemma']):
+        print('proccesing {} of {}'.format(row, reddit_df.shape[0]))
+        arguments={'keywords':" ".join(words), 
+                   'limit':n_images, 
+                   'format':'jpg',
+                   'prefix':str(row) + " ".join(words),
+                   'image_directory':os.path.join('images'), 
+                   'safe_search':True, 
+                   'print_urls':True}
+        absolute_image_paths = response.download(arguments)
+
+# Google the stem/lemma words, return
+#  first image link as a string
+def get_image_link( words, n_images=1,  ):
+    
+    # Set up directories we are working in
+    work_dir = os.getcwd()    
+    img_dir  = work_dir + '/downloads/images/'
+    log_dir  = work_dir + '/logs/'
+    
+    kw_str = " ".join(words)
+    
+    print(kw_str)
+    
+    # Get the image and metadata
+    response = google_images_download.googleimagesdownload()
+    arguments={'keywords':kw_str, 
+                   'limit':n_images, 
+                   'format':'jpg',
+                   'image_directory':os.path.join('images'), 
+                   'safe_search':True, 
+                   'extract_metadata':True,
+                   'no_numbering':True,
+                   'print_urls':True}
     absolute_image_paths = response.download(arguments)
+    
+    # Remove the image we downloaded, since we don't need it
+    print( list(absolute_image_paths.values())[0][0] )
+    downloaded_file = list(absolute_image_paths.values())[0][0]
+    os.remove( downloaded_file )
+
+    # The link to the image
+    img_path = ''
+
+    # Get data from log file
+    # start by opening the log file
+    with open( log_dir + kw_str + '.txt' , 'r' ) as f:
+        # Parse line by line, looking for "image_link"
+        for line in f:
+            line_arr = line.split(" ")
+            if ( '"image_link":' in line_arr ):
+                use_line = line
+                
+        line_arr = use_line.split(" ")
+        # File path is after image link
+        index = line_arr.index('"image_link":')
+
+        # Could in principle have link split across
+        #  indexes, so combine everything from here to end
+        for i in range( index+1, len(line_arr) ):
+            img_path += line_arr[i]
+
+        # Remove the quotes and \n character
+        img_path = img_path[1:-3]
+    print( img_path )
+    
+    os.remove( log_dir+kw_str+'.txt' )
+
+    return img_path
+
+
+if __name__ == '__main__':
+    main()
